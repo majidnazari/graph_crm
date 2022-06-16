021193fa0d0aff9d57a6023f0e8e0b05014a1daf
majid nazari <majidnazarister@gmail.com>
2022-06-16 12:55:29 +0430

    add passport to auth

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,15 +5,18 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
+use Laravel\Passport\Passport;
+
+
 class AuthServiceProvider extends ServiceProvider
-{
+{    
     /**
      * The policy mappings for the application.
      *
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -23,7 +26,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        
         $this->registerPolicies();
+        Passport::routes();
+
+        Passport::tokensExpireIn(now()->addDays(15));
+        
+        //$this->registerPolicies();
 
         Gate::define('parameters', function ($user) {
             $group = $user->group()->first();
