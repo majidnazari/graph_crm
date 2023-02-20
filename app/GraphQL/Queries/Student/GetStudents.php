674d6f5e3e0b0d814385706b6eff4c1a8af0c48e
majid nazari <majidnazarister@gmail.com>
2023-02-20 09:59:26 +0330

    show all is academy student in shown now

--- app/GraphQL/Queries/Student/GetStudents.php
+++ app/GraphQL/Queries/Student/GetStudents.php
@@ -30,15 +30,19 @@ final class GetStudents
         $phone = isset($args['phone']) ? $args['phone'] : "";
         // Log::info(json_encode($context->request()));
         $Student = Student::where('is_deleted', 0)
-        ->where('archived',0)
-        ->where('banned',0)
+            ->where('archived', 0)
+            ->where('banned', 0)
             ->where(DB::raw('CONCAT(first_name, \' \',last_name)'), 'like', "%" . $full_name . "%")
-            // ->where(function ($query) use ($args) {
-            //     if (isset($args['phone']) && ($args['phone']!="")) {
-            //         $query->where('phone','like',"%". $args['phone'] . "%");
-            //     }
-            //     return true;
-            // });
+            //->orWhere('is_academy_student1', 1)
+            ->orWhere(function ($query) use ($full_name) {
+                // if (isset($args['phone']) && ($args['phone']!="")) {
+                //     $query->where('phone','like',"%". $args['phone'] . "%");
+                // }
+                $query->where('is_academy_student', 1)
+                    //  ->where('archived', 0)
+                    //  ->where('banned', 0)
+                     ->where(DB::raw('CONCAT(first_name, \' \',last_name)'), 'like', "%" . $full_name . "%");
+            })
             ->where(function ($query) use ($phone) {
                 //Log::info("inner where in run");
                 if ($phone !== "") {
@@ -51,7 +55,6 @@ final class GetStudents
                         ->orWhere('mother_phone', 'like', "%" . $phone . "%")
                         ->orWhere('student_phone', 'like', "%" . $phone . "%");
                 }
-               
             });
 
 
